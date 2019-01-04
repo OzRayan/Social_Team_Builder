7a1d28dfe2ff21cbb46618fe3110e6a31c48c2fd
Feher Oszkar <feheroszkar@yahoo.com>
2019-01-04 04:07:10 +0100

    update

--- social_team_builder/projects/urls.py
+++ social_team_builder/projects/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'project/new/$', views.ProjectCreateView.as_view(), name='create'),
     url(r'project/(?P<pk>\d+)/$', views.ProjectDetailView.as_view(), name='detail'),
     url(r'project/(?P<pk>\d+)/delete/$', views.ProjectDeleteView.as_view(), name='delete'),
-    # url(r'project/(?P<pk>\d+)/edit/$', views.ProjectEditView.as_view(), name='edit'),
+    url(r'project/(?P<pk>\d+)/edit/$', views.ProjectEditView.as_view(), name='edit'),
 ]
